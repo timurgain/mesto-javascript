0aab1505d7102529c30f87b224a57091d3a86c4d
Timur Gain <timur.gain@gmail.com>
2022-11-13 11:50:22 +0300

    refactor: fillCardElement function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-// Constants
+// Global Constants
 
 const profilePopup = document.querySelector('.profile-popup');
 const profileForm = document.forms['profile'];
-const profileFormName = profileForm.elements['name']
-const profileFormDescription = profileForm.elements['description']
+const profileFormName = profileForm.elements['name'];
+const profileFormDescription = profileForm.elements['description'];
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const profileEditBtn = document.querySelector('.profile__edit-btn');
@@ -15,8 +15,12 @@ const placeFormName = placeForm.elements['name'];
 const placeFormLink = placeForm.elements['link'];
 
 const cardPopup = document.querySelector('.card-popup');
+const cardImagePopup = cardPopup.querySelector('.popup__image');
+const cardCaptionPopup = cardPopup.querySelector('.popup__caption');
 
-const buttonsPopupClose = document.querySelectorAll('.popup__close-btn')
+const cardList = document.querySelector('.elements__cards');
+
+const buttonsPopupClose = document.querySelectorAll('.popup__close-btn');
 
 // Functions
 
@@ -57,7 +61,6 @@ buttonsPopupClose.forEach((btn) => {
   })
 })
 
-
 profileEditBtn.addEventListener('click', () => {
   openPopup(profilePopup);
   fillFormProfile();
@@ -66,64 +69,47 @@ profileEditBtn.addEventListener('click', () => {
 profileForm.addEventListener('submit', (evt) => {
   saveFormProfileValues(); closePopup(profilePopup); evt.preventDefault()});
 
-
 placeAddBtn.addEventListener('click', () => openPopup(placePopup));
 
 placeForm.addEventListener('submit', (evt) => {
   const card = createCard(placeFormLink.value, placeFormName.value)
   addCardElementToCardList(card, where='prepend');
   closePopup(placePopup);
+  placeForm.reset();
   evt.preventDefault();
 })
 
-
-
-
 // Card and Card popup
-
-const popupCard = document.querySelector('#popup-card');
-const popupCardImage = popupCard.querySelector('.popup__image');
-const popupCardCaption = popupCard.querySelector('.popup__caption');
-
-
 
 function fillCardElement(cardElement, cardLink, cardName) {
   if (cardLink.length < 1 || cardName.length < 1) return alert('Пустые поля');
-
+  // constants
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardHeader = cardElement.querySelector('.card__header');
+  const cardLikeBtn = cardElement.querySelector('.card__like-btn');
+  const cardTrashBtn = cardElement.querySelector('.card__trash-btn')
   // attributes and text
-  cardElement.querySelector('.card__image').setAttribute('src', cardLink);
-  cardElement.querySelector('.card__header').textContent = cardName;
-
+  cardImage.setAttribute('src', cardLink);
+  cardImage.setAttribute('alt', `Изображение: ${cardName}`);
+  cardHeader.textContent = cardName;
   // event listeners
-  cardElement.querySelector('.card__like-btn').addEventListener(
+  cardLikeBtn.addEventListener(
     'click', (evt) => {evt.target.classList.toggle('card__like-btn_active')}
   );
-  cardElement.querySelector('.card__trash-btn').addEventListener(
-    'click', (evt) => {
-      const trashBtn = evt.target;
-      // trash-btn < figure < li
-      trashBtn.parentElement.parentElement.remove();
-    }
+  cardTrashBtn.addEventListener(
+    'click', (evt) => {evt.target.closest('li').remove()}
   );
-  cardElement.querySelector('.card__image').addEventListener(
-    'click', (evt) => {
-      // from card
-      const img = evt.target;
-      const parentElement = img.parentElement;
-      const placeLink = img.src;
-      const placeName = parentElement.querySelector('.card__header').textContent;
-      // into popup
-      popupCardImage.src = placeLink;
-      popupCardImage.alt = `Фотогафия ${placeName}`;
-      popupCardCaption.textContent = placeName;
-      // display popup
-      openPopup(popupCard);
+  cardImage.addEventListener(
+    'click', () => {
+      cardImagePopup.src = cardImage.src;
+      cardImagePopup.alt = cardImage.alt;
+      cardCaptionPopup.textContent = cardHeader.textContent;
+      openPopup(cardPopup);
     }
   );
 }
 
 function addCardElementToCardList(cardElement, where='append') {
-  const cardList = document.querySelector('.elements__cards');
   if (where === 'prepend') {
     cardList.prepend(cardElement);
   } else {
