5f01ea0a70522efb38693a2f31054978d03dccbe
Timur Gain <timur.gain@gmail.com>
2022-11-15 21:53:04 +0300

    feat: input validation layout

---
+++ scripts/validate.js
@@ -0,0 +1,11 @@
+const selectors = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-btn',
+  // no dots, use for classList
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
+
+
