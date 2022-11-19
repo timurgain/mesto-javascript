4de11464a1a677b33a9f2e045fbbdfd5d7eb3f00
Timur Gain <timur.gain@gmail.com>
2022-11-19 11:25:25 +0300

    refactor: handleEscape

--- scripts/index.js
+++ scripts/index.js
@@ -27,16 +27,16 @@ const popupSections = document.querySelectorAll('.popup');
 
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened');
-  document.addEventListener('keydown', escapeHandler);
+  document.addEventListener('keydown', handleEscape);
 
 }
 
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keydown', escapeHandler);
+  document.removeEventListener('keydown', handleEscape);
 }
 
-function escapeHandler(evt) {
+function handleEscape(evt) {
   if (evt.key !== 'Escape') return;
   const popup = document.querySelector('.popup_opened');
   if (!!popup === false) return;
