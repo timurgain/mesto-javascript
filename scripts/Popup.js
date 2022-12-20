ab161f889e27e6c1d851770bbb495a9ffe251aa8
Timur Gain <timur.gain@gmail.com>
2022-12-20 13:17:09 +0300

    fix: class Popup and _handleEscCloseBound for Listeners

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,16 +1,18 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscCloseBound = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened')
-    document.addEventListener('keydown', this._handleEscClose)
+    const handleEscCloseBinded = this._handleEscClose.bind(this)
+    document.addEventListener('keydown', this._handleEscCloseBound)
   }
 
   close() {
     this._popup.classList.remove('popup_opened')
-    document.removeEventListener('keydown', this._handleEscClose)
+    document.removeEventListener('keydown', this._handleEscCloseBound)
   }
 
   _handleEscClose(evt) {
@@ -26,5 +28,5 @@ export default class Popup {
       }
     })
   }
-  
+
 }
